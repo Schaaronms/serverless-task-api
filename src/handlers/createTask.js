59736c47aeff021c2022c857c3bc861399ade084
Schaaron Souza <schaaronms@gmail.com>
2026-06-19 15:59:49 -0300

    feat: add dynamodb module and project structure

---
+++ src/handlers/createTask.js
@@ -0,0 +1,12 @@
+{
+  "userId": "user-123",
+  "taskId": "task-001",
+  "title": "Estudar Terraform"
+}
+{
+  "userId": "user-123",
+  "taskId": "task-001",
+  "title": "Estudar Terraform",
+  "status": "pending",
+  "createdAt": "2026-06-19T18:00:00Z"
+}
